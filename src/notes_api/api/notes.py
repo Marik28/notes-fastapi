1d9279f79706a28a199ef739c8b501544b0bca6d
mark <markel20002000@gmail.com>
2021-07-29 21:56:08 +0600

    добавил новые эндпоинты

--- src/notes_api/api/notes.py
+++ src/notes_api/api/notes.py
@@ -1,10 +1,14 @@
+from uuid import UUID
+
 from fastapi import (
     APIRouter,
     Depends,
-    status
+    status,
+    Path,
+    Response
 )
 
-from ..models.notes import Note, NoteCreate
+from ..models.notes import Note, NoteCreate, NoteUpdate
 from ..models.users import User
 from ..services.auth import get_current_user
 from ..services.notes import NotesService
@@ -19,4 +23,23 @@ async def get_notes(service: NotesService = Depends(), user: User = Depends(get_
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_note(note_data: NoteCreate, service: NotesService = Depends(), user: User = Depends(get_current_user)):
-    service.create(user.id, note_data)
+    # todo сделать header location с полным путем до созданной заметки
+    service.create(note_data, user)
+
+
+@router.put("/{note_id}", status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+async def update_note(note_data: NoteUpdate, note_id: UUID = Path(...), service: NotesService = Depends(),
+                      user: User = Depends(get_current_user)):
+    service.update(note_id, note_data, user)
+
+
+@router.get("/{note_id}", response_model=Note)
+async def get_note(note_id: UUID = Path(...), service: NotesService = Depends(),
+                   user: User = Depends(get_current_user)):
+    return service.get(note_id, user)
+
+
+@router.delete("/{note_id}", status_code=status.HTTP_204_NO_CONTENT, response_class=Response)
+async def delete_note(note_id: UUID = Path(...), service: NotesService = Depends(),
+                      user: User = Depends(get_current_user)):
+    service.delete(note_id, user)
