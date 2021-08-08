30ab6ec5f8208efce52bedd082543ae0fe10998e
mark <markel20002000@gmail.com>
2021-08-08 15:07:03 +0600

    тесты тесты тесты

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -6,6 +6,9 @@ from notes_api.app import app
 
 client = TestClient(app)
 
+SIGN_UP_PATH = "/auth/sign-up/"
+SIGN_IN_PATH = "/auth/sign-in/"
+
 
 @pytest.fixture
 def fake_user():
@@ -17,12 +20,20 @@ def fake_user():
 
 
 def test_sigh_up_with_correct_data(fake_user):
-    response = client.post("/auth/sign-up/", json=fake_user)
+    response = client.post(SIGN_UP_PATH, json=fake_user)
     assert response.status_code == status.HTTP_201_CREATED
     assert "access_token" in response.json()
 
 
 def test_sign_in_with_correct_data(fake_user):
-    response = client.post("/auth/sign-in/", data=fake_user)
+    response = client.post(SIGN_IN_PATH, data=fake_user)
     assert response.status_code == status.HTTP_200_OK
     assert "access_token" in response.json()
+
+
+def test_create_user_with_existing_data(rnd_user_data):
+    response = client.post(SIGN_UP_PATH, json=rnd_user_data)
+    assert response.status_code == status.HTTP_201_CREATED
+    second_response = client.post(SIGN_UP_PATH, json=rnd_user_data)
+    assert second_response.status_code == status.HTTP_409_CONFLICT
+    assert "detail" in second_response.json()
--- tests/test_get_notes.py
+++ tests/test_get_notes.py
@@ -7,7 +7,12 @@ client = TestClient(app)
 GET_NOTES_PATH = "/notes/"
 
 
-def test_get_notes(token):
+def test_get_notes_successful_response(token):
     response = client.get(GET_NOTES_PATH, headers={"Authorization": token})
     assert response.status_code == status.HTTP_200_OK
     assert type(response.json()) is list
+
+
+def test_get_notes_unauthorized():
+    response = client.get(GET_NOTES_PATH)
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
