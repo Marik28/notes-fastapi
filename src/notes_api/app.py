873b8be093376a5978d5459c9da1659bad52b2d7
mark <markel20002000@gmail.com>
2021-08-23 16:17:36 +0600

    добавил cors middleware для деплоя

--- src/notes_api/app.py
+++ src/notes_api/app.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .api import router
+from .settings import settings
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
