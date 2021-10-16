f7ba4a981b937d590cb91ed4af937d4dd3c774c4
mark <markel20002000@gmail.com>
2021-10-16 21:59:13 +0600

    переписал скрипты при помощи typer

--- src/scripts.py
+++ src/scripts.py
@@ -1,29 +1,22 @@
-import argparse
+import typer
+
+app = typer.Typer()
 
 
+@app.command()
 def create_tables():
     from notes_api.database import engine
     from notes_api.tables import Base
     from notes_api.settings import settings
     Base.metadata.create_all(engine)
-    print(f"Таблицы в БД {settings.postgres_db_name} успешно созданы")
+    typer.echo(f"Таблицы в БД {settings.postgres_db_name} успешно созданы")
 
 
-def create_jwt_secret():
+@app.command()
+def create_jwt_secret(token_length: int = typer.Argument(32)):
     import secrets
-    print(secrets.token_urlsafe(32))
+    typer.echo(f"Токен сгенерирован:\n{secrets.token_urlsafe(token_length)}")
 
-
-available_commands = ["create-tables", "create-jwt-secret"]
-parser = argparse.ArgumentParser()
-parser.add_argument("command", help="Команда, которую необходимо выполнить", choices=available_commands)
-
-command_to_function = {
-    "create-tables": create_tables,
-    "create-jwt-secret": create_jwt_secret,
-}
 
 if __name__ == '__main__':
-    args = parser.parse_args()
-    print(args.command)
-    command_to_function[args.command]()
+    app()
